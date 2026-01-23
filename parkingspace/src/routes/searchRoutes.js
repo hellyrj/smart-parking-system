8dce9217f7a215a923af9366022ba3fb6085e766
Hilina <153182546+hellyrj@users.noreply.github.com>
2026-01-24 01:13:34 +0900

    Delete parkingspace/src/routes/searchRoutes.js

--- parkingspace/src/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { searchParking } from "../controllers/searchControllers.js";
-const router = express.Router();
-
-router.get("/search", searchParking);
-
-export default router;
