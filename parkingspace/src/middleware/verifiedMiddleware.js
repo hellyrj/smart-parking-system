d9f68491006cff8a5a25e0712c6ce1cb6dfa0a21
Hilina <153182546+hellyrj@users.noreply.github.com>
2026-01-24 01:12:57 +0900

    Delete parkingspace/src/middleware/verifiedMiddleware.js

--- parkingspace/src/middleware/verifiedMiddleware.js
+++
@@ -1,7 +0,0 @@
-// verifiedMiddleware.js (modified)
-const verifiedMiddleware = (req, res, next) => {
-  // Since all users are automatically verified, just continue
-  next();
-};
-
-export default verifiedMiddleware;
