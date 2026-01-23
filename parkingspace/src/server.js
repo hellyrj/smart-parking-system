214eb3a4b24e5442518db0388e760bec0ea5baf2
Hilina <153182546+hellyrj@users.noreply.github.com>
2026-01-24 00:57:12 +0900

    Delete parkingspace/src/server.js

--- parkingspace/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-//import sequelize from "./config/db.js";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Backend + Frontend running on http://localhost:${PORT}`);
-});
